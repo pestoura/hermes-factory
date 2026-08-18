5a553f6403cffbdb510acf9a6e2a46b5d51aa32e
Pedro <pedro.estoura@gmail.com>
2026-08-18 23:24:29 +0100

    chore: export JDS engineering profile reference

--- src/hermes_factory/contracts/__init__.py
+++ src/hermes_factory/contracts/__init__.py
@@ -1,3 +1,9 @@
+from .engineering import EngineeringProfileReference
 from .project import AcceptanceContract, ContractValidationError, ProjectContract
 
-__all__ = ["AcceptanceContract", "ContractValidationError", "ProjectContract"]
+__all__ = [
+    "AcceptanceContract",
+    "ContractValidationError",
+    "EngineeringProfileReference",
+    "ProjectContract",
+]
