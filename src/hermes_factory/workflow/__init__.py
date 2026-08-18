0919811128ae18d054b82e4b0c9785923c07885a
Pedro <pedro.estoura@gmail.com>
2026-08-18 23:56:58 +0100

    feat: export persistent HITL decision service

--- src/hermes_factory/workflow/__init__.py
+++ src/hermes_factory/workflow/__init__.py
@@ -1,10 +1,19 @@
 from .corrective import Finding, FindingClass, ReworkController, ReworkState
-from .hitl import HITLRequest, HITLState, HumanDecision, validate_human_decision
+from .hitl import (
+    HITLDecisionCommitError,
+    HITLDecisionService,
+    HITLRequest,
+    HITLState,
+    HumanDecision,
+    validate_human_decision,
+)
 from .uat import UATExecution, UATMode
 
 __all__ = [
     "Finding",
     "FindingClass",
+    "HITLDecisionCommitError",
+    "HITLDecisionService",
     "HITLRequest",
     "HITLState",
     "HumanDecision",
