f70974b2bd0ea0cf954df08e46dff95447d3630f
Pedro <pedro.estoura@gmail.com>
2026-08-18 23:37:10 +0100

    feat: expose Profile evaluation API skeleton

---
+++ src/hermes_factory/agents/evals.py
@@ -0,0 +1,18 @@
+class ProfileAdmissionError(ValueError):
+    pass
+
+
+class ProfileEvalEvidence:
+    pass
+
+
+class ProfileEvalHarness:
+    pass
+
+
+class ProfileEvalRecord:
+    pass
+
+
+class ProfileEvalState:
+    pass
