fca671514c2434a2a236a55012560cb244aa0574
Pedro <pedro.estoura@gmail.com>
2026-08-19 18:12:15 +0100

    feat: export northbound Factory control contract

---
+++ src/hermes_factory/control/__init__.py
@@ -0,0 +1,13 @@
+from hermes_factory.control.northbound import (
+    NorthboundAccessDenied,
+    NorthboundCaller,
+    NorthboundControl,
+    NorthboundOrigin,
+)
+
+__all__ = [
+    "NorthboundAccessDenied",
+    "NorthboundCaller",
+    "NorthboundControl",
+    "NorthboundOrigin",
+]
