2920e0c68e43e2bc00a1e96fa30d5b5c7d62bc3a
Pedro <pedro.estoura@gmail.com>
2026-08-19 18:16:01 +0100

    feat: export protected northbound mutation contract

--- src/hermes_factory/control/__init__.py
+++ src/hermes_factory/control/__init__.py
@@ -2,12 +2,16 @@ from hermes_factory.control.northbound import (
     NorthboundAccessDenied,
     NorthboundCaller,
     NorthboundControl,
+    NorthboundMutationDenied,
     NorthboundOrigin,
+    ProtectedMutationAction,
 )
 
 __all__ = [
     "NorthboundAccessDenied",
     "NorthboundCaller",
     "NorthboundControl",
+    "NorthboundMutationDenied",
     "NorthboundOrigin",
+    "ProtectedMutationAction",
 ]
